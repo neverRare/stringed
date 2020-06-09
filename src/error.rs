7df26b0b1018504620d0b834d88946f39dcd6649
neverRare <35887047+neverRare@users.noreply.github.com>
2020-06-09 20:28:40 +0800

    added unexpect

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,6 @@
 pub fn expect(expected: &str, found: &str) -> String {
     format!("expected {}, found {}", expected, found)
 }
+pub fn unexpect(unexpected: &str) -> String {
+    format!("unexpected {}", unexpected)
+}
