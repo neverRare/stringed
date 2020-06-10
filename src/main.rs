db7e8ee4cacb4f2fbed4fe3301afdaf9cdfa9359
neverRare <35887047+neverRare@users.noreply.github.com>
2020-06-10 14:29:38 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+use stringed::Interpretter;
+
 fn main() {
-    println!("Hello, world!");
+    let mut interpretter = Interpretter::new(
+        || {
+            let mut guess = String::new();
+            if let Err(reason) = io::stdin().read_line(&mut guess) {
+                Err(reason.to_string())
+            } else {
+                Ok(guess)
+            }
+        },
+        |string| {
+            println!("{}", string);
+        },
+    );
+    let code = r#"
+"Please enter your name:
+" + "Hello " + ? + "!"
+"#;
+    if let Err(reason) = interpretter.run(code) {
+        eprintln!("Error: {}", reason);
+    }
 }
