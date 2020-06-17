c9881adb2ddb6dc91a286558e0f00ff535ac2f46
neverRare <35887047+neverRare@users.noreply.github.com>
2020-06-17 20:55:26 +0800

    added output queue

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -4,3 +4,14 @@ pub fn parse_uint(a: &str) -> Result<usize, String> {
         Err(reason) => Err(reason.to_string()),
     }
 }
+pub fn find_newline(string: &str) -> Option<usize> {
+    match (
+        string.find('\n').map(|i| i + 1),
+        string.find("\r\n").map(|i| i + 2),
+    ) {
+        (Some(lf), Some(crlf)) => Some(lf.min(crlf)),
+        (Some(lf), None) => Some(lf),
+        (None, Some(crlf)) => Some(crlf),
+        (None, None) => None,
+    }
+}
