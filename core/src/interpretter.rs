2b483821246edd4c911111974956cd262f8ef106
neverRare <35887047+neverRare@users.noreply.github.com>
2020-06-18 13:56:17 +0800

    simplified interpretter

--- core/src/interpretter.rs
+++ core/src/interpretter.rs
@@ -1,5 +1,5 @@
-use crate::utils::parse_uint;
-use crate::parser::Node;
+use crate::gen_interpretter::{GenInterpretter, Output};
+use crate::output_queue::OutputQueue;
 
 pub struct Interpretter<I, O>
 where
@@ -8,7 +8,6 @@ where
 {
     input: I,
     output: O,
-    queue: String,
 }
 impl<I, O> Interpretter<I, O>
 where
@@ -16,81 +15,38 @@ where
     O: Fn(&str) -> (),
 {
     pub fn new(input: I, output: O) -> Self {
-        Self {
-            input,
-            output,
-            queue: String::new(),
-        }
-    }
-    fn queue_output(&mut self, string: &str) {
-        self.queue.push_str(string);
-        let mut lines: Vec<&str> = self.queue.lines().collect();
-        let new_queue = if self.queue.ends_with('\n') || lines.is_empty() {
-            ""
-        } else {
-            lines.pop().unwrap()
-        };
-        for line in lines {
-            (self.output)(line);
-        }
-        self.queue = new_queue.to_string();
-    }
-    fn run_node(&mut self, var: &str, node: &Node) -> Result<(), String> {
-        match node {
-            Node::Group(node) => self.run_node(var, node)?,
-            Node::Closure { left, right } => self.run_node(&self.eval(var, left)?, right)?,
-            Node::Concat(vec) => {
-                for node in vec {
-                    self.run_node(var, node)?;
-                }
-            }
-            Node::Eval(node) => self.run_node(var, &Node::parse(&self.eval(var, node)?)?)?,
-            node => self.queue_output(&self.eval(var, node)?),
-        }
-        Ok(())
-    }
-    fn eval(&self, var: &str, node: &Node) -> Result<String, String> {
-        match node {
-            Node::Literal(content) => Ok(content.to_string()),
-            Node::Group(node) => self.eval(var, node),
-            Node::Prompt => Ok((self.input)()),
-            Node::Var => Ok(var.to_string()),
-            Node::Closure { left, right } => self.eval(&self.eval(var, left)?, right),
-            Node::Concat(vec) => {
-                let mut val = String::new();
-                for node in vec {
-                    val.push_str(&self.eval(var, node)?);
-                }
-                Ok(val)
-            }
-            Node::Slice { src, lower, upper } => {
-                let src = self.eval(var, src)?;
-                let lower = match lower {
-                    Some(node) => parse_uint(&self.eval(var, node)?)?,
-                    None => 0,
-                };
-                let upper = match upper {
-                    Some(node) => parse_uint(&self.eval(var, node)?)?,
-                    None => src.len(),
-                };
-                if upper > src.len() {
-                    Err("upper bound larger than the length of string".to_string())
-                } else if lower > upper {
-                    Err("lower bound larger than upper bound".to_string())
-                } else {
-                    Ok(src[lower..upper].to_string())
-                }
-            }
-            Node::Equal { left, right } => {
-                Ok((self.eval(var, left)? == self.eval(var, right)?).to_string())
-            }
-            Node::Length(node) => Ok((self.eval(var, node)?).len().to_string()),
-            Node::Eval(node) => Ok(self.eval(var, &Node::parse(&self.eval(var, node)?)?)?),
-        }
+        Self { input, output }
     }
     pub fn run(&mut self, src: &str) -> Result<(), String> {
-        self.run_node("", &Node::parse(src)?)?;
-        (self.output)(&self.queue);
-        Ok(())
+        let mut interpretter = GenInterpretter::start(src);
+        let mut queue = OutputQueue::new();
+        let mut result = None;
+        loop {
+            let input;
+            if let Some(result) = result {
+                match result {
+                    Output::Output(output) => {
+                        for output in queue.insert(&output) {
+                            (self.output)(&output);
+                        }
+                        input = None;
+                    }
+                    Output::Input => {
+                        input = Some((self.input)());
+                    }
+                    Output::Error(reason) => break Err(reason),
+                    Output::Done => {
+                        (self.output)(&queue.left());
+                        break Ok(());
+                    }
+                }
+            } else {
+                input = None;
+            }
+            result = Some(interpretter.next(match &input {
+                Some(x) => Some(&x),
+                None => None,
+            }));
+        }
     }
 }
